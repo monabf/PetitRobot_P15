6495699aff12c1073be42acce523af8149e4bf7e
Mona Buisson-Fenet <mona.buissonfenet@me.com>
2017-03-28 17:22:01 +0200

    Corrected strategy and added some comments about rotations, integrated color sensor

--- CPetitBras.cs
+++ CPetitBras.cs
@@ -3,17 +3,22 @@ using Microsoft.SPOT;
 
 namespace RobotEve
 {
-    class PetitBras
+    class CPetitBras
     {
 
         Couleur equipe;
         CAX12 m_ax12PetitBras;
+        CRouletteIntelligente m_rouletteIntelligente;
 
 
         struct configPetitBras
         {
             public byte idAX12PetitBrasBleu;   //mouvement de rentré et sortie du bras
+            public byte idAx12RotateurBleu;
+            public byte idCapteurBrasBleu;
             public byte idAX12PetitBrasJaune;   //mouvement de rentré et sortie du bras
+            public byte idAx12RotateurJaune;
+            public byte idCapteurBrasJaune;
         };
 
         enum positionPetitBrasBleu
@@ -22,37 +27,45 @@ namespace RobotEve
             intermediaire_rentree = 0,
             sortie = 512,
             intermediaire_sortie = 125 // Valeurs à changer après tests sur servos
-
         };
 
         enum positionPetitBrasJaune
-         {
-        rentree=177,
-        intermediaire_rentree= 0,
-        sortie=512,
-        intermediaire_sortie= 125 // Valeurs à changer après tests sur servos
-
-         };
-
+        {
+            rentree=177,
+            intermediaire_rentree= 0,
+            sortie=512,
+            intermediaire_sortie= 125 // Valeurs à changer après tests sur servos
+        };
 
 
-        public PetitBras(Couleur equipe, ControleurAX12 controleur, configPetitBras config) //le constructeur
+
+        public CPetitBras(Couleur equipe, ControleurAX12 controleur, configPetitBras config) //le constructeur
         {
             // ce n'est pas superbe, mais c'est efficace !
             if (equipe == Couleur.Jaune)
             {
                 m_ax12PetitBras = new CAX12(config.idAX12PetitBrasJaune, controleur.m_port, controleur.m_direction);
+                CCapteurCouleur capteurCouleurJaune = new CCapteurCouleur(config.idCapteurBrasJaune);
+                CAX_12 ax12RotateurJaune = new CAX_12(config.idAx12RotateurJaune, controleur.m_port, controleur.m_direction);
+                m_ax12Poussoir.setMode(CAX_12.AX12Mode.joint);
+                m_rouletteIntelligente = CRouletteIntelligente(capteurCouleurJaune, ax12RotateurJaune);
+                m_ax12Poussoir = new CAX_12(config.idAx12Poussoir, controleur.m_port, controleur.m_direction);
             }
             else
             {
                 m_ax12PetitBras = new CAX12(config.idAX12PetitBrasBleu, controleur.m_port, controleur.m_direction);
+                CCapteurCouleur capteurCouleurBleu = new CCapteurCouleur(config.idCapteurBrasBleu);
+                CAX_12 ax12RotateurBleu = new CAX_12(config.idAx12Rotateur, controleur.m_port, controleur.m_direction);
+                m_ax12Poussoir.setMode(CAX_12.AX12Mode.joint);
+                m_rouletteIntelligente = CRouletteIntelligente(capteurCouleurBleu, ax12RotateurBleu);
+                m_ax12Poussoir = new CAX_12(config.idAx12Poussoir, controleur.m_port, controleur.m_direction);
             }
             m_ax12PetitBras.setMode(AX12Mode.joint);
 
         }
 
 
-        public void deplie()
+        public void deplie(Couleur equipe)
         {
             if (equipe == Couleur.Jaune)
             {
@@ -64,7 +77,7 @@ namespace RobotEve
             }
         }
 
-        public void replie()
+        public void replie(Couleur equipe)
         {
             if (equipe == Couleur.Jaune)
             {
@@ -77,7 +90,7 @@ namespace RobotEve
 
         }
 
-        public void semiReplie()
+        public void semiReplie(Couleur equipe)
         {
             if (equipe == Couleur.Jaune)
             {
@@ -90,7 +103,7 @@ namespace RobotEve
         }
 
 
-        public void semiDeplie()
+        public void semiDeplie(Couleur equipe)
         {
             if (equipe == Couleur.Jaune)
             {
@@ -100,6 +113,11 @@ namespace RobotEve
             {
                 m_ax12PetitBras.move((int)positionPetitBrasBleu.intermediaire_sortie);
             }
+        }
+
+        public void tourner(Couleur equipe)
+        {
+            m_rouletteIntelligente.mettreBonneCouleur(equipe);
         }
 
 
---
+++ Poussoir.cs
@@ -0,0 +1,75 @@
+using System;
+using Microsoft.SPOT;
+
+namespace RobotEve
+{
+    class Poussoir
+    {
+
+      Couleur couleurEquipe;
+      CAX12 m_ax12Poussoir;
+
+
+    struct configPoussoir
+    {
+        public byte idAX12PoussoirBleu;
+        public byte idAX12PoussoirJaune;
+     };
+
+    enum positionPoussoirBleu
+    {
+        rentree=20,
+        sortie=20,
+       // Valeurs a changer apres tests sur servos
+    };
+
+    enum positionPoussoirJaune
+    {
+        rentree=20,
+        sortie=20,
+       // Valeurs a changer apres tests sur servos
+    };
+
+
+        public Poussoir(couleurEquipe, ControleurAX12 controleur,configPoussoir config) //le constructeur
+        {
+
+          if (equipe == Couleur.Jaune)
+          {
+            m_ax12Poussoir = new CAX12(config.idAX12PoussoirJaune, controleur.m_port, controleur.m_direction);
+          }
+          else
+          {
+            m_ax12Poussoir = new CAX12(config.idAX12PoussoirBleu, controleur.m_port, controleur.m_direction);
+          }
+           m_ax12Poussoir.setMode(AX12Mode.joint);
+        }
+
+
+        public void rotate()
+        {
+          if (equipe == Couleur.Jaune)
+          {
+              m_ax12Poussoir.move((int)positionPoussoirJaune.sortie);
+          }
+
+          else
+          {
+              m_ax12Poussoir.move((int)positionPoussoirBleu.sortie);
+          }
+        }
+
+        public void replie()
+        {
+          if (equipe == Couleur.Jaune){
+            m_ax12Poussoir.move((int)positionPoussoirJaune.rentree);
+          }
+
+          else {
+              m_ax12Poussoir.move((int)positionPoussoirBleu.rentree);
+          }
+        }
+
+
+    }
+}
